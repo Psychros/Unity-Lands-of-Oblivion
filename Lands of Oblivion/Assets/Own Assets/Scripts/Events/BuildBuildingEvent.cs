130a4922e70f1a3b33a6a86818dfadc064c8656a
lord349123 <t-ho@web.de>
2015-08-04 00:27:44 +0200

    Added Ocean Ground
    The ocean has now a terrain for the ground.

--- Lands of Oblivion/Assets/Own Assets/Scripts/Events/BuildBuildingEvent.cs
+++ Lands of Oblivion/Assets/Own Assets/Scripts/Events/BuildBuildingEvent.cs
@@ -4,10 +4,12 @@ using System.Collections;
 
 public class BuildBuildingEvent : UserEvent {
 
+	private static GameObject building;
+
 	public void execute(){
 		if(SetBuildingPositionController.instance.building == null){
 			//Get the position where the player is looking at the terrain
-			RaycastHit hit = RayCastManager.startRayCast(15);
+			RaycastHit hit = RayCastManager.startRayCast(50);
 			
 			
 			//Buildings can only be build on terrains
@@ -15,10 +17,9 @@ public class BuildBuildingEvent : UserEvent {
 				if(hit.transform.gameObject.tag == "Terrain"){
 					Vector3 pos = hit.point;
 					
-					GameObject building = Instantiate(BuildingManager.instance.storeHouse);
-					print (BuildingManager.instance);
+					building = Instantiate(BuildingManager.instance.storeHouse);
 					building.transform.position = pos;
-					
+
 					SetBuildingPositionController.instance.building = building;
 				}
 			} 
@@ -27,6 +28,33 @@ public class BuildBuildingEvent : UserEvent {
 			}
 		} else{
 			SetBuildingPositionController.instance.building = null;
+
+			if(building != null)
+				adjustTerrain(building);
 		}
+	}
+
+
+	//Adjust the terrain under the building
+	private void adjustTerrain(GameObject building){
+		TerrainData data = Terrain.activeTerrain.terrainData;
+		BoxCollider collider = building.GetComponent<BoxCollider>();
+		Vector3 pos = collider.transform.position;
+		Vector3 size = collider.size;
+
+		print (pos.x + ", " + pos.z);
+		int x = (int)(pos.x/4);	
+		int z = (int)(pos.z-(size.z/4));	
+		float[,] area = new float[(int)(size.z), (int)(size.x)];
+
+		//Fill the are with the height of the building
+		for(int i=0; i<area.GetLength(0); i++){
+			for(int j=0; j<area.GetLength(1); j++){
+				area[i, j] = pos.y/200;
+			}
+		}
+
+		//Make the change visible
+		data.SetHeights(x/4, z/4, area);
 	}
 }
