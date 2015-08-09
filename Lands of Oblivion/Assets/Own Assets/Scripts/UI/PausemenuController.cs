8d84034b2414302abfa08dfa02f4f219a39ed883
lord349123 <t-ho@web.de>
2015-08-09 19:29:08 +0200

    Added pausemenu

---
+++ Lands of Oblivion/Assets/Own Assets/Scripts/UI/PausemenuController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PausemenuController : MonoBehaviour {
+
+	public void continueGame(){
+		InputManager.instance.switchToMenu(InputManager.instance.pausemenuCanvas, false);
+	}
+
+	public void startmenu(){
+		Application.LoadLevel("Startmenu");
+	}
+
+	public void quitGame(){
+		Application.Quit();
+	}
+}
