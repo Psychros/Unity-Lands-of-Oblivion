9becd64af88cf35de2ce6748c6faf526355ae022
Crest <emailadressezweite@gmail.com>
2015-10-17 01:18:06 +0100

    Removed cause for exception
    Exception was caused by endless recursion due to the wrong usage of the
    special getters and setters in C#.

--- Lands of Oblivion/Assets/Own Assets/Scripts/LevelScripts/Chunk.cs
+++ Lands of Oblivion/Assets/Own Assets/Scripts/LevelScripts/Chunk.cs
@@ -5,13 +5,17 @@ using System.Text;
 using UnityEngine;
 
 
+
 class Chunk
 {
-    public List<GameObject> list { get { return new List<GameObject>(list); } private set { list = value; } }
+
+    private List<GameObject> prvList;
+
+    public List<GameObject> list { get { return new List<GameObject>(prvList); }}
 
     public Chunk(List<GameObject> list)
     {
-        this.list = new List<GameObject>();
+        this.prvList= new List<GameObject>();
 
         foreach (GameObject tempObj in list)
         {
@@ -21,18 +25,18 @@ class Chunk
 
     public Chunk(GameObject obj)
     {
-        this.list = new List<GameObject>();
+        this.prvList = new List<GameObject>();
         this.add(obj);
     }
 
     public Chunk()
     {
-        this.list = new List<GameObject>();
+        this.prvList = new List<GameObject>();
     }
 
     public Boolean add(GameObject obj)
     {
-        if (!this.list.Contains(obj) && obj.GetComponent<MeshFilter>().mesh != null)
+        if (!this.list.Contains(obj) && obj.GetComponent<MeshFilter>() != null)
         {
             this.list.Add(obj);
             return true;
