c6d1ee75edd3600f1a68e2f323da11f74ccbbf85
Crest <emailadressezweite@gmail.com>
2015-10-17 23:26:56 +0100

    Implemented class for general useful stuff

---
+++ Lands of Oblivion/Assets/Own Assets/Scripts/Util.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+class Util
+{
+    public static Boolean contains<T, U>(T obj, U[] array)
+    {
+        if (obj == null && array == null) return true;
+        if (obj == null || array == null) return false;
+        Boolean returned = false;
+        foreach (U u in array)
+        {
+            if (Object.Equals(u, obj))
+            {
+                returned = true;
+                break;
+            }
+        }
+        return returned;
+    }
+}
+
