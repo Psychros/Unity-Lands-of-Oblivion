ce292a7bce1ba28a7ba244b335e6e24441a9a672
Crest <emailadressezweite@gmail.com>
2015-10-21 16:31:54 +0100

    Added very useful Exception

---
+++ Lands of Oblivion/Assets/Own Assets/Scripts/IllegalArgumentException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class IllegalArgumentException : Exception
+{
+    public IllegalArgumentException(String str) : base(str) { }
+}
